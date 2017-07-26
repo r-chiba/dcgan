aac76413a6050719b4ad6f75bb47bdbc5d284591
Chiba Ryuichiro <chiba.r.aa@m.titech.ac.jp>
2017-07-26 18:02:28 +0900

    Use Class

--- model.py
+++ model.py
@@ -11,9 +11,103 @@ import cv2
 
 from utils import *
 
+
+class Genarator:
+    def __init__(self, batch_size, in_d, out_h, out_w, out_c, dim, name='generator', reuse=False):
+        self.batch_size = batch_size
+        self.dim = dim
+        h2, w2 = pad_out_size_same(out_h, 2), pad_out_size_same(out_w, 2)
+        h4, w4 = pad_out_size_same(h2, 2), pad_out_size_same(w2, 2)
+        h8, w8 = pad_out_size_same(h4, 2), pad_out_size_same(w4, 2)
+        h16, w16 = pad_out_size_same(h8, 2), pad_out_size_same(w8, 2)
+        self.reshape = (h16, w16)
+
+        with tf.variable_scope(name) as scope:
+            if reuse == True:
+                scope.reuse_variables()
+
+            self.linear = Linear(in_d, self.dim*8*h16*w16, name='g_h0')
+
+            self.deconv1 = Deconvolution2d(self.dim*8, [self.batch_size, h8, w8, self.dim*4], sth=2, stw=2, name='g_h1')
+            self.bn1 = BatchNormalization()
+
+            self.deconv2 = Deconvolution2d(self.dim*4, [self.batch_size, h4, w4, self.dim*2], sth=2, stw=2, name='g_h2')
+            self.bn2 = BatchNormalization()
+            
+            self.deconv3 = Deconvolution2d(self.dim*2, [self.batch_size, h2, w2, self.dim], sth=2, stw=2, name='g_h3')
+            self.bn3 = BatchNormalization()
+
+            self.deconv4 = Deconvolution2d(self.dim, [self.batch_size, h, w, out_c], sth=2, stw=2, name='g_h4')
+            self.bn4 = BatchNormalization()
+
+
+    def __call__(self, z, train=True):
+        h = self.linear(z)
+        h = tf.nn.relu(h)
+        h = tf.reshape(h, [self.batch_size, self.reshape[0], self.reshape[1], self.dim*8])
+
+        h = self.deconv1(h)
+        h = self.bn1(h, train=train)
+        h = tf.nn.relu(h)
+
+        h = self.deconv2(h)
+        h = self.bn2(h, train=train)
+        h = tf.nn.relu(h)
+
+        h = self.deconv3(h)
+        h = self.bn3(h, train=train)
+        h = tf.nn.relu(h)
+
+        h = self.deconv4(h)
+        h = self.bn4(h, train=train)
+        h = tf.nn.relu(h)
+
+        return tf.nn.tanh(h)
+
+
+class Discriminator:
+    def __init__(self, batch_size, in_c, in_h, in_w, dim, name='Discriminator', reuse=False):
+        self.batch_size = batch_size
+        self.dim = dim
+        h2, w2 = pad_out_size_same(in_h, 2), pad_out_size_same(in_w, 2)
+        h4, w4 = pad_out_size_same(h2, 2), pad_out_size_same(w2, 2)
+        h8, w8 = pad_out_size_same(h4, 2), pad_out_size_same(w4, 2)
+
+        with tf.variable_scope(name) as scope:
+            if reuse == True:
+                scope.reuse_variables()
+
+            self.conv1 = Convolution2d(in_c, self.dim*2, sth=2, stw=2, name='d_h0')
+            self.bn1 = BatchNormalization()
+
+            self.conv2 = Convolution2d(self.dim*2, self.dim*4, sth=2, stw=2, name='d_h1')
+            self.bn2 = BatchNormalization()
+
+            self.conv3 = Convolution2d(self.dim*4, self.dim*8, sth=2, stw=2, name='d_h2')
+            self.bn3 = BatchNormalization()
+
+            self.linear = Linear(h8*w8*self.dim*8, 1, name='d_h3')
+
+
+    def __call__(self, x, train=True):
+        h = self.conv1(x)
+        h = self.bn1(h)
+        h = lrelu(h)
+
+        h = self.conv2(x)
+        h = self.bn2(h)
+        h = lrelu(h)
+
+        h = self.conv3(x)
+        h = self.bn3(h)
+        h = lrelu(h)
+
+        logit = self.linear(h)
+
+        return logit
+
+
 class GAN:
-    #def __init__(self, sess, batch_size, input_height, input_width, n_channel, 
-    #            output_height, output_width, z_dim, g_dim, d_dim, dataset_name):
     def __init__(self, sess, flags):
         self.sess = sess
         self.batch_size = flags.batch_size
@@ -26,118 +120,23 @@ class GAN:
         self.g_dim = flags.g_dim
         self.d_dim = flags.d_dim
         self.learning_rate = flags.learning_rate
-        #self.momentum = flags.momentum
         self.beta1 = flags.beta1
         self.savedir = flags.save_dir
         self.training = flags.train
         self.n_epoch = flags.n_epoch
 
-    def generator(self, z, reuse=False, training=True):
-        h, w = self.output_height, self.output_width
-        h2, w2 = pad_out_size_same(h, 2), pad_out_size_same(w, 2)
-        h4, w4 = pad_out_size_same(h2, 2), pad_out_size_same(w2, 2)
-        h8, w8 = pad_out_size_same(h4, 2), pad_out_size_same(w4, 2)
-        h16, w16 = pad_out_size_same(h8, 2), pad_out_size_same(w8, 2)
+        self.Gen = Generator(self.batch_size, self.n_channel, self.output_height, self.output_width, self.n_channel, self.g_dim)
+        self.Dis = Discriminator(self.batch_size, self.n_channel, self.output_height, self.output_width, self.d_dim)
+        #self.Dis_real = Discriminator(self.batch_size, self.n_channel, self.output_height, self.output_width, self.d_dim)
+        #self.Dis_fake = Discriminator(self.batch_size, self.n_channel, self.output_height, self.output_width, self.d_dim, reuse=True)
 
-        with tf.variable_scope('generator') as scope:
-            if reuse == True:
-                scope.reuse_variables()
-
-            hid0, self.h0_w, self.h0_b = linear(z, self.g_dim*8*h16*w16, 'g_h0_lin', with_w=True)
-            #hid0 = tf.contrib.layers.batch_norm(hid0, decay=0.9,
-            #    updates_collections=None, epsilon=1e-5, scale=True, is_training=training)
-            hid0 = tf.nn.relu(hid0)
-            hid0 = tf.reshape(hid0, [self.batch_size, h16, w16, self.g_dim*8])
-
-            hid1, self.h1_w, self.h1_b = deconv2d(hid0, [self.batch_size, h8, w8, self.g_dim*4], 
-                sth=2, stw=2, name='g_h1', with_w=True)
-            hid1 = tf.contrib.layers.batch_norm(hid1, decay=0.9,
-                updates_collections=None, epsilon=1e-5, scale=True, is_training=training)
-            hid1 = tf.nn.relu(hid1)
-
-            hid2, self.h2_w, self.h2_b = deconv2d(hid1, [self.batch_size, h4, w4, self.g_dim*2], 
-                sth=2, stw=2, name='g_h2', with_w=True)
-            hid2 = tf.contrib.layers.batch_norm(hid2, decay=0.9,
-                updates_collections=None, epsilon=1e-5, scale=True, is_training=training)
-            hid2 = tf.nn.relu(hid2)
-
-            hid3, self.h3_w, self.h3_b = deconv2d(hid2, [self.batch_size, h2, w2, self.g_dim], 
-                sth=2, stw=2, name='g_h3', with_w=True)
-            hid3 = tf.contrib.layers.batch_norm(hid3, decay=0.9,
-                updates_collections=None, epsilon=1e-5, scale=True, is_training=training)
-            hid3 = tf.nn.relu(hid3)
-
-            hid4, self.h4_w, self.h4_b = deconv2d(hid3, [self.batch_size, h, w, self.n_channel], 
-                sth=2, stw=2, name='g_h4', with_w=True)
-
-            if reuse == True:
-                tf.histogram_summary('g_h0_w', self.h0_w)
-                tf.histogram_summary('g_h0_b', self.h0_b)
-                tf.histogram_summary('g_h1_w', self.h1_w)
-                tf.histogram_summary('g_h1_b', self.h1_b)
-                tf.histogram_summary('g_h2_w', self.h2_w)
-                tf.histogram_summary('g_h2_b', self.h2_b)
-                tf.histogram_summary('g_h3_w', self.h3_w)
-                tf.histogram_summary('g_h3_b', self.h3_b)
-                tf.histogram_summary('g_h4_w', self.h4_w)
-                tf.histogram_summary('g_h4_b', self.h4_b)
-
-            return tf.nn.tanh(hid4)
-
-    def discriminator(self, x, reuse=False):
-        with tf.variable_scope('discriminator') as scope:
-            if reuse == True:
-                scope.reuse_variables()
-
-            h0, self.h0_w, self.h0_b= conv2d(x, self.d_dim, sth=2, stw=2, 
-                name='d_h0_conv', with_w=True)
-            h0 = tf.contrib.layers.batch_norm(h0, decay=0.9,
-                updates_collections=None, epsilon=1e-5, scale=True, is_training=self.training)
-            h0 = lrelu(h0)
-
-            h1, self.h1_w, self.h1_b= conv2d(h0, self.d_dim*2, sth=2, stw=2, 
-                name='d_h1_conv', with_w=True)
-            h1 = tf.contrib.layers.batch_norm(h1, decay=0.9,
-                updates_collections=None, epsilon=1e-5, scale=True, is_training=self.training)
-            h1 = lrelu(h1)
-
-            h2, self.h2_w, self.h2_b= conv2d(h1, self.d_dim*4, sth=2, stw=2,
-                name='d_h2_conv', with_w=True)
-            h2 = tf.contrib.layers.batch_norm(h2, decay=0.9,
-                updates_collections=None, epsilon=1e-5, scale=True, is_training=self.training)
-            h2 = lrelu(h2)
-            
-            h3, self.h3_w, self.h3_b= conv2d(h2, self.d_dim*8, sth=2, stw=2, 
-                name='d_h3_conv', with_w=True)
-            h3 = tf.contrib.layers.batch_norm(h3, decay=0.9,
-                updates_collections=None, epsilon=1e-5, scale=True, is_training=self.training)
-            h3 = lrelu(h3)
-
-            h4,self.h4_w, self.h4_b = linear(tf.reshape(h3, [self.batch_size, -1]),
-                1, 'd_h4_lin', with_w=True)
-
-            if reuse == False:
-                tf.histogram_summary('d_h0_w', self.h0_w)
-                tf.histogram_summary('d_h0_b', self.h0_b)
-                tf.histogram_summary('d_h1_w', self.h1_w)
-                tf.histogram_summary('d_h1_b', self.h1_b)
-                tf.histogram_summary('d_h2_w', self.h2_w)
-                tf.histogram_summary('d_h2_b', self.h2_b)
-                tf.histogram_summary('d_h3_w', self.h3_w)
-                tf.histogram_summary('d_h3_b', self.h3_b)
-                tf.histogram_summary('d_h4_w', self.h4_w)
-                tf.histogram_summary('d_h4_b', self.h4_b)
-
-            return h4
-
-    def build_model(self):
         self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
         self.x_real = tf.placeholder(tf.float32, 
             [self.batch_size, self.input_height, self.input_width, self.n_channel], name='x')
-        self.x_fake = self.generator(self.z)
-        self.x_sample = self.generator(self.z, reuse=True, training=False)
-        self.d_real = self.discriminator(self.x_real)
-        self.d_fake = self.discriminator(self.x_fake, reuse=True)
+        self.x_fake = self.Gen(self.z)
+        self.x_sample = self.Gen(self.z, training=False)
+        self.d_real = self.Dis(self.x_real)
+        self.d_fake = self.Dis(self.x_fake, reuse=True)
 
         self.d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
             logits=self.d_real, targets=tf.ones_like(self.d_real)))
@@ -163,6 +162,138 @@ class GAN:
         self.saver = tf.train.Saver()
         self.summary = tf.merge_all_summaries()
         self.writer = tf.train.SummaryWriter(self.savedir, self.sess.graph)
+
+    #def generator(self, z, reuse=False, training=True):
+    #    h, w = self.output_height, self.output_width
+    #    h2, w2 = pad_out_size_same(h, 2), pad_out_size_same(w, 2)
+    #    h4, w4 = pad_out_size_same(h2, 2), pad_out_size_same(w2, 2)
+    #    h8, w8 = pad_out_size_same(h4, 2), pad_out_size_same(w4, 2)
+    #    h16, w16 = pad_out_size_same(h8, 2), pad_out_size_same(w8, 2)
+
+    #    with tf.variable_scope('generator') as scope:
+    #        if reuse == True:
+    #            scope.reuse_variables()
+
+    #        hid0, self.h0_w, self.h0_b = linear(z, self.g_dim*8*h16*w16, 'g_h0_lin', with_w=True)
+    #        #hid0 = tf.contrib.layers.batch_norm(hid0, decay=0.9,
+    #        #    updates_collections=None, epsilon=1e-5, scale=True, is_training=training)
+    #        hid0 = tf.nn.relu(hid0)
+    #        hid0 = tf.reshape(hid0, [self.batch_size, h16, w16, self.g_dim*8])
+
+    #        hid1, self.h1_w, self.h1_b = deconv2d(hid0, [self.batch_size, h8, w8, self.g_dim*4], 
+    #            sth=2, stw=2, name='g_h1', with_w=True)
+    #        hid1 = tf.contrib.layers.batch_norm(hid1, decay=0.9,
+    #            updates_collections=None, epsilon=1e-5, scale=True, is_training=training)
+    #        hid1 = tf.nn.relu(hid1)
+
+    #        hid2, self.h2_w, self.h2_b = deconv2d(hid1, [self.batch_size, h4, w4, self.g_dim*2], 
+    #            sth=2, stw=2, name='g_h2', with_w=True)
+    #        hid2 = tf.contrib.layers.batch_norm(hid2, decay=0.9,
+    #            updates_collections=None, epsilon=1e-5, scale=True, is_training=training)
+    #        hid2 = tf.nn.relu(hid2)
+
+    #        hid3, self.h3_w, self.h3_b = deconv2d(hid2, [self.batch_size, h2, w2, self.g_dim], 
+    #            sth=2, stw=2, name='g_h3', with_w=True)
+    #        hid3 = tf.contrib.layers.batch_norm(hid3, decay=0.9,
+    #            updates_collections=None, epsilon=1e-5, scale=True, is_training=training)
+    #        hid3 = tf.nn.relu(hid3)
+
+    #        hid4, self.h4_w, self.h4_b = deconv2d(hid3, [self.batch_size, h, w, self.n_channel], 
+    #            sth=2, stw=2, name='g_h4', with_w=True)
+
+    #        if reuse == True:
+    #            tf.histogram_summary('g_h0_w', self.h0_w)
+    #            tf.histogram_summary('g_h0_b', self.h0_b)
+    #            tf.histogram_summary('g_h1_w', self.h1_w)
+    #            tf.histogram_summary('g_h1_b', self.h1_b)
+    #            tf.histogram_summary('g_h2_w', self.h2_w)
+    #            tf.histogram_summary('g_h2_b', self.h2_b)
+    #            tf.histogram_summary('g_h3_w', self.h3_w)
+    #            tf.histogram_summary('g_h3_b', self.h3_b)
+    #            tf.histogram_summary('g_h4_w', self.h4_w)
+    #            tf.histogram_summary('g_h4_b', self.h4_b)
+
+    #        return tf.nn.tanh(hid4)
+
+    #def discriminator(self, x, reuse=False):
+    #    with tf.variable_scope('discriminator') as scope:
+    #        if reuse == True:
+    #            scope.reuse_variables()
+
+    #        h0, self.h0_w, self.h0_b= conv2d(x, self.d_dim, sth=2, stw=2, 
+    #            name='d_h0_conv', with_w=True)
+    #        h0 = tf.contrib.layers.batch_norm(h0, decay=0.9,
+    #            updates_collections=None, epsilon=1e-5, scale=True, is_training=self.training)
+    #        h0 = lrelu(h0)
+
+    #        h1, self.h1_w, self.h1_b= conv2d(h0, self.d_dim*2, sth=2, stw=2, 
+    #            name='d_h1_conv', with_w=True)
+    #        h1 = tf.contrib.layers.batch_norm(h1, decay=0.9,
+    #            updates_collections=None, epsilon=1e-5, scale=True, is_training=self.training)
+    #        h1 = lrelu(h1)
+
+    #        h2, self.h2_w, self.h2_b= conv2d(h1, self.d_dim*4, sth=2, stw=2,
+    #            name='d_h2_conv', with_w=True)
+    #        h2 = tf.contrib.layers.batch_norm(h2, decay=0.9,
+    #            updates_collections=None, epsilon=1e-5, scale=True, is_training=self.training)
+    #        h2 = lrelu(h2)
+    #        
+    #        h3, self.h3_w, self.h3_b= conv2d(h2, self.d_dim*8, sth=2, stw=2, 
+    #            name='d_h3_conv', with_w=True)
+    #        h3 = tf.contrib.layers.batch_norm(h3, decay=0.9,
+    #            updates_collections=None, epsilon=1e-5, scale=True, is_training=self.training)
+    #        h3 = lrelu(h3)
+
+    #        h4,self.h4_w, self.h4_b = linear(tf.reshape(h3, [self.batch_size, -1]),
+    #            1, 'd_h4_lin', with_w=True)
+
+    #        if reuse == False:
+    #            tf.histogram_summary('d_h0_w', self.h0_w)
+    #            tf.histogram_summary('d_h0_b', self.h0_b)
+    #            tf.histogram_summary('d_h1_w', self.h1_w)
+    #            tf.histogram_summary('d_h1_b', self.h1_b)
+    #            tf.histogram_summary('d_h2_w', self.h2_w)
+    #            tf.histogram_summary('d_h2_b', self.h2_b)
+    #            tf.histogram_summary('d_h3_w', self.h3_w)
+    #            tf.histogram_summary('d_h3_b', self.h3_b)
+    #            tf.histogram_summary('d_h4_w', self.h4_w)
+    #            tf.histogram_summary('d_h4_b', self.h4_b)
+
+    #        return h4
+
+    #def build_model(self):
+    #    self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
+    #    self.x_real = tf.placeholder(tf.float32, 
+    #        [self.batch_size, self.input_height, self.input_width, self.n_channel], name='x')
+    #    self.x_fake = self.generator(self.z)
+    #    self.x_sample = self.generator(self.z, reuse=True, training=False)
+    #    self.d_real = self.discriminator(self.x_real)
+    #    self.d_fake = self.discriminator(self.x_fake, reuse=True)
+
+    #    self.d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+    #        logits=self.d_real, targets=tf.ones_like(self.d_real)))
+    #    self.d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+    #        logits=self.d_fake, targets=tf.zeros_like(self.d_fake)))
+    #    self.d_loss = self.d_loss_real + self.d_loss_fake
+    #    self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+    #        logits=self.d_fake, targets=tf.ones_like(self.d_fake)))
+
+    #    self.d_vars = [x for x in tf.trainable_variables() if 'd_' in x.name]
+    #    self.g_vars = [x for x in tf.trainable_variables() if 'g_' in x.name]
+
+    #    self.d_optimizer = tf.train.AdamOptimizer(self.learning_rate, beta1=self.beta1).minimize(
+    #        self.d_loss, var_list=self.d_vars)
+    #    self.g_optimizer = tf.train.AdamOptimizer(self.learning_rate, beta1=self.beta1).minimize(
+    #        self.g_loss, var_list=self.g_vars)
+
+    #    tf.scalar_summary('d_loss_real', self.d_loss_real)
+    #    tf.scalar_summary('d_loss_fake', self.d_loss_fake)
+    #    tf.scalar_summary('d_loss', self.d_loss)
+    #    tf.scalar_summary('g_loss', self.g_loss)
+
+    #    self.saver = tf.train.Saver()
+    #    self.summary = tf.merge_all_summaries()
+    #    self.writer = tf.train.SummaryWriter(self.savedir, self.sess.graph)
 
     def train(self, batch_generator):
         def tile_image(imgs):
--- utils.py
+++ utils.py
@@ -3,6 +3,78 @@ from __future__ import print_function
 import math
 import tensorflow as tf
 
+def lrelu(x, leak=0.2):
+    return tf.maximum(x, leak*x)
+
+
+def pad_out_size_same(in_size, stride):
+    return int(math.ceil(float(in_size) / float(stride)))
+
+
+def pad_out_size_valid(in_size, filter_size, stride):
+    return int(math.ceil(float(in_size - filter_size + 1) / float(stride)))
+
+
+class Linear:
+    def __init__(self, in_d, out_d, sd=0.02, bias_init=0.0, name='linear'):
+        with tf.variable_scope(name):
+            self.w = tf.get_variable('w', [in_d, out_d], tf.float32,
+                tf.truncated_normal_initializer(stddev=sd))
+            self.b = tf.get_variable('b', [out_d], initializer=tf.constant_initializer(bias_init))
+
+
+    def __call__(self, x):
+        return tf.matmul(x, self.w) + self.b
+
+
+    def get_variable(self):
+        return self.w, self.b
+
+
+class Convolution2d:
+    def __init__(self, in_c, out_c, kh=5, kw=5, sth=1, stw=1, sd=0.02, name='conv2d'):
+        self.sth = sth
+        self.stw = stw
+
+        with tf.variable_scope(name):
+            self.w = tf.get_variable('w', [kh, kw, in_c, out_c],
+                initializer=tf.truncated_normal_initializer(stddev=sd))
+            self.b = tf.get_variable('b', [out_c], initializer=tf.constant_initializer(0.0))
+
+
+    def __call__(self, x):
+        ret = tf.nn.conv2d(x, w, strides=[1, self.sth, self.stw, 1], padding='SAME')
+        ret = tf.reshape(tf.nn.bias_add(ret, bias), ret.get_shape())
+        return ret
+     
+
+    def get_variable(self):
+        return self.w, self.b
+
+
+class Deconvolution2d:
+    def __init__(self, in_c, out_shape, kh=5, kw=5, sth=1, stw=1, sd=0.02, name='deconv2d'):
+        self.out_shape = out_shape
+        self.sth = sth
+        self.stw = stw
+
+        with tf.variable_scope(name):
+            self.w = tf.get_variable('w', [kh, kw, out_shape[-1], in_c],
+                    initializer=tf.truncated_normal_initializer(stddev=sd))
+            self.b = tf.get_variable('b', [out_shape[-1]], initializer=tf.constant_initializer(0.0))
+
+
+    def __call__(self, x):
+        ret = tf.nn.conv2d_transpose(x, self.w, output_shape=self.out_shape,
+                    strides=[1, self.sth, self.stw, 1])
+        ret = tf.reshape(tf.nn.bias_add(ret, self.b), ret.get_shape())
+        return ret
+     
+
+    def get_variable(self):
+        return self.w, self.b
+
+
 class BatchNormalization:
     def __init__(self, epsilon=1e-5, decay=0.9, name='batch_norm'):
         self.epsilon = epsilon
@@ -13,47 +85,40 @@ class BatchNormalization:
         return tf.contrib.layers.batch_norm(x, decay=self.decay, updates_collections=None,
             epsilon=self.epsilon, scale=True, is_training=train, scope=self.name)
 
-def pad_out_size_same(in_size, stride):
-    return int(math.ceil(float(in_size) / float(stride)))
 
-def pad_out_size_valid(in_size, filter_size, stride):
-    return int(math.ceil(float(in_size - filter_size + 1) / float(stride)))
+#def linear(input_, output_size, vs_name='Linear', sd=0.02, bias_start=0.0, with_w=False):
+#    shape = input_.get_shape().as_list()
+#    with tf.variable_scope(vs_name):
+#        w = tf.get_variable('w', [shape[1], output_size], tf.float32,
+#                tf.random_normal_initializer(stddev=sd))
+#        b = tf.get_variable('b', [output_size], initializer=tf.constant_initializer(bias_start))
+#        if with_w == True:
+#            return tf.matmul(input_, w) + b, w, b
+#        else:
+#            return tf.matmul(input_, w) + b
+#
+#def conv2d(input_, output_dim, kh=5, kw=5, sth=1, stw=1, sd=0.02, name='conv2d', with_w=False):
+#    with tf.variable_scope(name):
+#        w = tf.get_variable('w', [kh, kw, input_.get_shape()[-1], output_dim],
+#                initializer=tf.truncated_normal_initializer(stddev=sd))
+#        conv = tf.nn.conv2d(input_, w, strides=[1, sth, stw, 1], padding='SAME')
+#        bias = tf.get_variable('bias', [output_dim], initializer=tf.constant_initializer(0.0))
+#        conv = tf.reshape(tf.nn.bias_add(conv, bias), conv.get_shape())
+#        if with_w == True:
+#            return conv, w, bias
+#        else:
+#            return conv
+#
+#def deconv2d(input_, output_shape, kh=5, kw=5, sth=1, stw=1, sd=0.02, name='deconv2d', with_w=False):
+#    with tf.variable_scope(name):
+#        w = tf.get_variable('w', [kh, kw, output_shape[-1], input_.get_shape()[-1]],
+#                initializer=tf.truncated_normal_initializer(stddev=sd))
+#        deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+#                    strides=[1, sth, stw, 1])
+#        bias = tf.get_variable('bias', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+#        conv = tf.reshape(tf.nn.bias_add(deconv, bias), deconv.get_shape())
+#        if with_w == True:
+#            return deconv, w, bias
+#        else:
+#            return deconv
 
-def conv2d(input_, output_dim, kh=5, kw=5, sth=1, stw=1, sd=0.02, name='conv2d', with_w=False):
-    with tf.variable_scope(name):
-        w = tf.get_variable('w', [kh, kw, input_.get_shape()[-1], output_dim],
-                initializer=tf.truncated_normal_initializer(stddev=sd))
-        conv = tf.nn.conv2d(input_, w, strides=[1, sth, stw, 1], padding='SAME')
-        bias = tf.get_variable('bias', [output_dim], initializer=tf.constant_initializer(0.0))
-        conv = tf.reshape(tf.nn.bias_add(conv, bias), conv.get_shape())
-        if with_w == True:
-            return conv, w, bias
-        else:
-            return conv
-
-def deconv2d(input_, output_shape, kh=5, kw=5, sth=1, stw=1, sd=0.02, name='deconv2d', with_w=False):
-    with tf.variable_scope(name):
-        w = tf.get_variable('w', [kh, kw, output_shape[-1], input_.get_shape()[-1]],
-                initializer=tf.truncated_normal_initializer(stddev=sd))
-        deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
-                    strides=[1, sth, stw, 1])
-        bias = tf.get_variable('bias', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
-        conv = tf.reshape(tf.nn.bias_add(deconv, bias), deconv.get_shape())
-        if with_w == True:
-            return deconv, w, bias
-        else:
-            return deconv
-
-def lrelu(x, leak=0.2, name='lrelu'):
-    return tf.maximum(x, leak*x)
-
-def linear(input_, output_size, vs_name='Linear', sd=0.02, bias_start=0.0, with_w=False):
-    shape = input_.get_shape().as_list()
-    with tf.variable_scope(vs_name):
-        w = tf.get_variable('w', [shape[1], output_size], tf.float32,
-                tf.random_normal_initializer(stddev=sd))
-        b = tf.get_variable('b', [output_size], initializer=tf.constant_initializer(bias_start))
-        if with_w == True:
-            return tf.matmul(input_, w) + b, w, b
-        else:
-            return tf.matmul(input_, w) + b
